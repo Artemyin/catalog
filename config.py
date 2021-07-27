ef27dc201d73b1b383e78ae3c8c1142fb841ad79
Artemii <artemyiyin@gmail.com>
2021-07-27 12:33:52 +0300

    change config sqla_track_mod to false

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
+
+#FLASK_APP = 'app'
+#FLASK_ENV= 'development'
+
+ADMINS = frozenset(['youremail@yourdomain.com'])
+SECRET_KEY = 'SecretKeyForSessionSigning'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_CONNECT_OPTIONS = {}
