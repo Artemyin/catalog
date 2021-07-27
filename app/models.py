ae66af341cba5439e4cf15b2d08006e40911d987
Artemiy Noskov <Artemyin@gmail.com>
2021-07-27 19:28:09 +0000

    add address model

--- app/models.py
+++ app/models.py
@@ -1,29 +1,43 @@
 from . import db
 
+class Address(db.Model):
+    __tablename__ = 'address'
+
+    id = db.Column(db.Integer, primary_key=True)
+    street_id = db.relationship('Street', backref='address', lazy=True)
+    building_id = db.relationship('Building', backref='address', lazy=True)
+    entrance_id = db.relationship('Entrance', backref='address', lazy=True)
+
 class Street(db.Model):
     __tablename__ = 'street'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
-    buildings = db.relationship('Building', backref='street', lazy=True)
-
+    address_id = db.Column(db.Integer, db.ForeignKey('address.id'),
+        nullable=False)
+    #buildings = db.relationship('Building', backref='street', lazy=True)
+     
 
 class Building(db.Model):
     __tablename__ = 'building'
 
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.String(100), unique=True, nullable=False)
-    street_name = db.Column(db.Integer, db.ForeignKey('street.id'),
+    address_id = db.Column(db.Integer, db.ForeignKey('address.id'),
         nullable=False)
-    entrances = = db.relationship('Entrance', backref='building', lazy=True)
+    #street_name = db.Column(db.Integer, db.ForeignKey('street.id'),
+    #    nullable=False)
+    #entrances = = db.relationship('Entrance', backref='building', lazy=True)
 
 class Entrance(db.Model):
     __tablename__ = 'entrance'
 
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.Integer, unique=True, nullable=False)
-    building_number = db.Column(db.Integer, db.ForeignKey('building.id'),
-        nullable=False)
+    address_id = db.Column(db.Integer, db.ForeignKey('address.id'),
+        nullable=False) 
+    #building_number = db.Column(db.Integer, db.ForeignKey('building.id'),
+    #    nullable=False)
 
 ''''
 class Floor(db.Model):
