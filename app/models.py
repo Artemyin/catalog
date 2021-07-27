0017b529593f5fadeb3fa0f46a832716e6c4008b
Artemiy Noskov <Artemyin@gmail.com>
2021-07-27 17:29:43 +0000

    fill models

--- app/models.py
+++ app/models.py
@@ -2,18 +2,35 @@ from . import db
 
 class Street(db.Model):
     __tablename__ = 'street'
-
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+     
 
 class Building(db.Model):
     __tablename__ = 'building'
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(100), unique=True, nullable=False)
 
 
 class Entrance(db.Model):
     __tablename__ = 'entrance'
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, unique=True, nullable=False)
 
+
+''''
 class Floor(db.Model):
     __tablename__ = 'floor'
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, unique=True, nullable=False)
+'''
 
 
 class Lamp(db.Model):
     __tablename__ = "lamp"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    power = db.Column(db.Integer)
+    #color
+    #type
+    #meanlife
