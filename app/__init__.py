93234a768f5e9796d59d37a8d2f7e09c2dfe0f0c
Artemii <artemyiyin@gmail.com>
2021-07-28 11:22:31 +0300

    add views and admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+#from flask_admin import Admin
 
 
 app = Flask(__name__)
+
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
+#admin = Admin(app, template_mode='bootstrap3')
+
+from app.models import Address, Lamp, Street, Building, Entrance
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+
+admin = Admin(app, template_mode='bootstrap3')
+
+admin.add_view(ModelView(Address, db.session))
+admin.add_view(ModelView(Street, db.session))
+admin.add_view(ModelView(Building, db.session))
+admin.add_view(ModelView(Entrance, db.session))
+admin.add_view(ModelView(Lamp, db.session))
